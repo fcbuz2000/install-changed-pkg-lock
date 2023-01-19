adb8dfd935b96d7ea000833015aeca6e63e91193
fcb2000 <vectorxy977@gmail.com>
2023-01-19 17:49:23 +0500

    update: shell type changed

--- bin/install-changed.js
+++ bin/install-changed.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
-const { program } = require("commander");
+const { program } = require('commander')
 
-const watcher = require("../lib/main");
+const watcher = require('../lib/main')
 
 program
   .option(
-    "--install-command [command]",
-    "The command to run when dependencies need to be installed/updated"
+    '--install-command [command]',
+    'The command to run when dependencies need to be installed/updated'
   )
+// .option(
+//   '--shell',
+//   'shell type for exec'
+// )
   .option(
-    "--hash-filename [filename]",
-    "Filename where hash of dependencies will be written to"
+    '--hash-filename [filename]',
+    'Filename where hash of dependencies will be written to'
   )
-  .option("--lock-filename [lock_filename]", "Filename of package lock")
-  .option("--hash-only", "Only update the hash");
+  .option('--lock-filename [lock_filename]', 'Filename of package lock')
+  .option('--hash-only', 'Only update the hash')
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 watcher({
   installCommand: program.installCommand,
   hashFilename: program.hashFilename,
   lockFilename: program.lockFilename,
-  isHashOnly: program.hashOnly,
-});
+  isHashOnly: program.hashOnly
+})
